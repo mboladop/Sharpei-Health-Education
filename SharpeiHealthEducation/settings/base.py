c27c64e9d9d7f8e8c37b4789058bf675ec888374
mboladop <mboladop@gmail.com>
2018-09-12 19:37:36 +0100

    DB change and custom storages created

--- SharpeiHealthEducation/settings/base.py
+++ SharpeiHealthEducation/settings/base.py
@@ -1,4 +1,5 @@
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -60,12 +61,16 @@ WSGI_APPLICATION = 'SharpeiHealthEducation.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
     }
-}
 
 
 # Password validation
