500c3165ee503c77dd899b05f0bac7aff4e6003c
mboladop <mboladop@gmail.com>
2018-09-13 14:26:35 +0100

    home app and staticfiles

--- SharpeiHealthEducation/settings/base.py
+++ SharpeiHealthEducation/settings/base.py
@@ -1,10 +1,10 @@
 import os
 import dj_database_url
 
-#SSL HACK to collectstatic on prod
+# #SSL HACK to collectstatic on prod
+
 if hasattr(ssl, '_create_unverified_context'):
     ssl._create_default_https_context = ssl._create_unverified_context
-
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
